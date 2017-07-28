e8799a832b88fff2223863dd5d0e26ea85becc9b
Ernie Yang <ernieyang09@hotmail.com>
2017-07-28 23:52:32 +0800

    新增繼續閱讀按鈕 增加post snippet css

--- src/components/PostItem/PostItem.jsx
+++ src/components/PostItem/PostItem.jsx
@@ -2,6 +2,36 @@ import React from "react"
 import Link from "gatsby-link"
 import styled from 'styled-components'
 
+const Post = styled.div`
+  padding-bottom: 40px;
+`;
+
+const PostSnippet = styled.div`
+  clear: left;
+  font-size: 15px;
+  line-height: 1.77;
+  color: #444;
+  padding-top: 15px;
+  text-align: justify;
+  text-justify: distribute;
+  word-break: normal;
+`;
+
+const BtnLink = styled(Link)`
+  font-size: 14px;
+  color: #444;
+  margin-top: 10px;
+  padding: 5px 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  float: right;
+  &:hover {
+    background: #F8F8F8;
+    border-bottom-color: #D26911;
+  }
+`;
+
+
 export default class PostItem extends React.Component {
   render() {
         // <snippet>[^]*?<\/snippet>
@@ -9,12 +39,12 @@ export default class PostItem extends React.Component {
     const snippet = props.html.match(/<snippet>[^]*?<\/snippet>/)[0];
 
     return (
-      <div>
+      <Post>
         <h3><Link to={props.fields.slug}>{props.frontmatter.title}</Link></h3>
         <h4>{props.frontmatter.date}</h4>
-        <div  dangerouslySetInnerHTML={{ __html: snippet }} />
-        <Link to={props.fields.slug}>繼續閱讀</Link>
-      </div>
+        <PostSnippet  dangerouslySetInnerHTML={{ __html: snippet }} />
+        <BtnLink to={props.fields.slug}>繼續閱讀</BtnLink>
+      </Post>
     )
   }
 }
