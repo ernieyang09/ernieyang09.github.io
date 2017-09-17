92d4e6af4e402fe59291be71d91a678c840e4e82
Ernie Yang <ernieyang09@hotmail.com>
2017-09-17 17:31:06 +0800

    add about init

---
+++ src/pages/about/index.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import Link from "gatsby-link"
+import Helmet from "react-helmet"
+
+export default class About extends React.Component {
+  render() {
+    return (
+      <div className='content-container'>
+
+        <div>github: https://github.com/ernieyang09/</div>
+        <div>Profolio: https://ernieyang09.github.io/Portfolio/</div>
+      </div>
+    )
+  }
+}
